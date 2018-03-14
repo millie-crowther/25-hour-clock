2e9f50459b8153d037e89652bbe61ccca9278bd1
millie <amelia.g.crowther@gmail.com>
2018-03-14 22:29:24 +0000

    tidied up code and made a pin mapping schema

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import time
 import sys
 
-rate = 0.4
+# update rate for clock state
+rate = 0.45
+
+# pin mapping
+#   + first index is digit number: hours = {0, 1}, minutes = {2, 3}, seconds = {4, 5}
+#   + letters correspond to LEDs as defined in data sheet
+#   + numbers correspond to GPIO pins on Pi
+pin_map = {
+    0 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 },
+    1 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 },
+    2 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 },
+    3 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 },
+    4 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 },
+    5 : { 'A' : 0, 'B' : 0, 'C' : 0, 'D' : 0, 'E' : 0, 'F' : 0, 'G' : 0 }
+}
 
 def start_time(from_disk):
+    # returns the time the clock started
+
     # try loading from disk
     if from_disk:
         try:
@@ -21,35 +37,23 @@ def start_time(from_disk):
     f.close()
     return result
 
-def get_uptime(start_time):
-    # returns time since startup - possibly use more accurate internet clock?
-    result = time.time()
-    return int(result) - start_time
-
-def get_digits(start_time):
+def get_digits(t):
     # converts uptime to an array of digits
-    t = get_uptime(start_time)
-    h = str((t / 3600) % 24)
-    m = str((t / 60) % 60)
-    s = str(t % 60)
-    
-    h = h.rjust(2, '0') 
-    m = m.rjust(2, '0') 
-    s = s.rjust(2, '0')
+    h = str((t / 3600) % 24).rjust(2, '0')
+    m = str((t / 60) % 60).rjust(2, '0')
+    s = str(t % 60).rjust(2, '0')
 
-    result = [h[0], h[1], m[0], m[1], s[0], s[1]] 
-    return [int(n) for n in result]
+    return [int(n) for n in h + m + s]
 
-def bit_map(digits):
-    # transforms full state of clock into nineteen-element binary vector
-    bits_req = [2, 3, 3, 4, 3, 4]
-    
-    result = []
-    for (i, digit) in enumerate(digits):
-        result += [digit & (1 << shift) != 0 for shift in range(bits_req[i])]
-    return result 
+def get_pin_map(led_map):
+    # returns a full pin map for all digits this tick
+    result = {}
+    for digit in range(6):
+        for led, value in led_map[digit].items():
+            result[pin_map[digit][led]] = value
+    return result
 
-def led_map(digits):
+def get_led_map(digits):
     # creates a mapping from LEDs on the display to whether or not they are active
     l = lambda digit:  {
         'A' : digit in [0, 2, 3, 5, 6, 8, 9],
@@ -65,11 +69,14 @@ def led_map(digits):
 
 def main():
     t0 = start_time('--load' in sys.argv)
+
     while True:
-        digits = get_digits(t0)
+        t = int(time.time()) - t0
+        digits = get_digits(t)
+    
         print digits
-        print bit_map(digits)
-        print led_map(digits)
+        print get_led_map(digits)
+
         time.sleep(rate)
 
 if __name__ == "__main__":
